e6f42a1da39f7032ed290523fc83f99e9d1c9b7d
PiperAtDawn <master_zayats@mail.ru>
2022-02-02 15:07:56 +0300

    Add substring method

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,18 @@
+def substrings (sentence, dictionary)
+  array = sentence.downcase.split(" ")
+  result = {}
+  dictionary.each do |d|
+    occurences = 0
+    array.each do |word|
+        occurences = occurences + 1 if word.include?(d)
+    end
+    result[d] = occurences if occurences > 0
+  end
+  result
+end
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+sentence = "Howdy partner, sit down! How's it going?"
+
+puts substrings(sentence, dictionary)
